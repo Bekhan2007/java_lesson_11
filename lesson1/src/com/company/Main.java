9f177500f8867a88d51ad06c744e2ddface49e76
Bekhan2007 <110276840+Bekhan2007@users.noreply.github.com>
2022-07-30 02:39:13 -0400

    Add files via upload

---
+++ lesson1/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Osiyo";
+        int age = 14;
+        int your = 2008;
+        String phone = "+998940805815";
+        String address = "pop";
+        String school = "1-maktab ";
+
+
+        System.out.println("malumotlar");
+        System.out.println("name " + name);
+        System.out.println("age " + age);
+        System.out.println("your:"+your);
+        System.out.println("phone "+phone);
+        System.out.println("address "+address);
+        System.out.println("school "+school);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
